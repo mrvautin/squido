6a174439157c8891cbb5f44503f1791a7798694b
Mark Moffat <hi@markmoffat.com>
2021-04-24 20:18:49 +0930

    Enhancements to the /squido admin

--- admin/assets/javascripts/admin.js
+++ admin/assets/javascripts/admin.js
@@ -19,8 +19,10 @@ document.addEventListener('DOMContentLoaded', () => {
               'Content-Type': 'application/json'
             },
             body: JSON.stringify({
+                postId: document.getElementById('post-id').value,
                 permalink: document.getElementById('post-permalink').value,
                 title: document.getElementById('post-title').value,
+                description: document.getElementById('post-description').value,
                 markdown: mdContent
             })
         })
@@ -30,6 +32,52 @@ document.addEventListener('DOMContentLoaded', () => {
                 return;
             }
             vNotify.success({ text: 'Successfully updated post', title: 'Success', position: 'topRight' });
+        });
+    });
+
+    const htmlRegex = /<.+?>/g;
+    const searchInput = document.getElementById('search-input');
+    const searchResults = document.getElementById('search-results');
+    searchInput.addEventListener('keyup', async (e) => {
+        if(searchInput.value.length > 2){
+            // clear results
+            searchResults.innerHTML = '';
+
+            // Search
+            const results = await fetch('/squido/search', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    searchTerm: searchInput.value
+                })
+            });
+
+            const data = await results.json();
+            // Display results
+            let resultCount = 0;
+            data.forEach((result) => {
+                if(resultCount < 10){
+                    const searchregex = new RegExp(`(${searchInput.value})`);
+                    const textsearch = result.body.replace(htmlRegex, '').search(searchregex);
+                    const foundtext = result.body.replace(htmlRegex, '').substring(textsearch - 40, (textsearch + searchInput.value.length) + 40);
+                    const highlightedText = foundtext.replace(searchInput.value, `<span class="text-squido-highlight">${searchInput.value}</span>`);
+                    const contents = `<p><span class="result-title"> ${result.title}</span><br/><small>${highlightedText}</small></p></a>`;
+                    searchResults.innerHTML = `${searchResults.innerHTML}<a class='list-group-item search-result-item' href='/squido/${result.id}'>${contents}</a>`;
+                    resultCount++;
+                }
+            });
+        }else{
+            searchResults.innerHTML = '';
+        }
+
+        // When the clear "x" is clicked
+        searchInput.addEventListener('search', (e) => {
+            if(searchInput.value === ''){
+                // clear results
+                searchResults.innerHTML = '';
+            }
         });
     });
 }, false);
--- admin/routes/index.js
+++ admin/routes/index.js
@@ -2,18 +2,26 @@ const express = require('express');
 const fs = require('fs');
 const yaml = require('js-yaml');
 const chalk = require('chalk');
+const FlexSearch = require('flexsearch');
 const _ = require('lodash');
 const router = express.Router();
 
+// Setup post index
+const htmlRegex = /<.+?>/g;
+const index = new FlexSearch();
+for(const post of process.postList){
+    index.add(post.id, post.body.replace(htmlRegex, ''));
+}
+
 router.get('/squido', async (req, res) => {
     const firstPost = process.postList[0];
-    res.redirect(`/squido/${firstPost.permalink}`);
+    res.redirect(`/squido/${firstPost.id}`);
 });
 
 router.get('/squido/:post', async (req, res) => {
-    const postPermalink = req.params.post;
+    const postId = req.params.post;
     const posts = process.postList;
-    const post = posts.find(p => p.permalink === postPermalink);
+    const post = posts.find(p => p.id === postId);
 
     // Check for post
     if(!post){
@@ -29,13 +37,39 @@ router.get('/squido/:post', async (req, res) => {
     });
 });
 
+router.post('/squido/search', async (req, res) => {
+    const results = await index.search({
+        query: req.body.searchTerm,
+        async: false,
+        limit: 10,
+        encode: 'icase',
+        tokenize: 'full',
+        worker: 4
+    });
+
+    const filteredResults = _.chain(process.postList)
+    .keyBy('id')
+    .at(results)
+    .value();
+
+    res.status(200).json(filteredResults);
+});
+
 router.post('/squido/save', async (req, res) => {
     const posts = process.postList;
-    const postIndex = _.findIndex(posts, (o) => { return o.permalink === req.body.permalink; });
+    const postIndex = _.findIndex(posts, (o) => { return o.id === req.body.postId; });
     const post = posts[postIndex];
     // Check for title change and update
     if(post.fileMeta.title !== req.body.title){
         post.fileMeta.title = req.body.title;
+    }
+    // Check for permalink change and update
+    if(post.fileMeta.permalink !== req.body.permalink){
+        post.fileMeta.permalink = req.body.permalink;
+    }
+    // Check for description change and update
+    if(post.fileMeta.description !== req.body.description){
+        post.fileMeta.description = req.body.description;
     }
     const meta = yaml.dump(post.fileMeta);
     const updatedPost =
@@ -49,6 +83,8 @@ ${req.body.markdown}
         // Update values to memory
         process.postList[postIndex].markdown = req.body.markdown;
         process.postList[postIndex].title = req.body.title;
+        process.postList[postIndex].permalink = req.body.permalink;
+        process.postList[postIndex].description = req.body.description;
         fs.writeFileSync(post.filename, updatedPost);
         res.status(200).json({});
     }catch(ex){
