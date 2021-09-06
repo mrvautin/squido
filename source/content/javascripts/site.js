2db6bbd7adec0faff5c9ef47cd7778ce02a6f2a2
Mark Moffat <hi@markmoffat.com>
2021-09-06 13:26:17 +0930

    Swagger template enhancements

--- source/content/javascripts/site.js
+++ source/content/javascripts/site.js
@@ -3,4 +3,30 @@ document.addEventListener('DOMContentLoaded', function () {
   for (var i = 0; i < images.length; i++) {
     images[i].classList.add('img-fluid');
   }
+
+  // Scroll to top
+  const scrollToTopButton = document.getElementById('js-top');
+  const scrollFunc = () => {
+    let y = window.scrollY;
+    if (y > 0) {
+      scrollToTopButton.className = "top-link show";
+    } else {
+      scrollToTopButton.className = "top-link hide";
+    }
+  };
+
+  window.addEventListener("scroll", scrollFunc);
+
+  const scrollToTop = () => {    
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  // When the button is clicked, run our ScrolltoTop function above!
+  scrollToTopButton.onclick = function(e) {
+    e.preventDefault();
+    scrollToTop();
+  }
 }, false);
